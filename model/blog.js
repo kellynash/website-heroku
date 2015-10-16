caabc9d0752aab38bcb4f2c3db0f57fc1f7fbc82
ed42311 <ed42311@gmail.com>
2015-10-16 10:09:56 -0600

    server errot while using put command ('Can\'t set headers after they are sent.')

---
+++ model/blog.js
@@ -0,0 +1,9 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BlogSchema   = new Schema({
+    title: String,
+    body: String
+});
+
+module.exports = mongoose.model('Blog', BlogSchema);
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var db = require('./model/db');
+var blogModel = require('./model/blog');
+
+var app = express();
+
+var blogRoutes = require('./routes/blog');
+
+app.use(express.static('public'));
+
+app.use('/api/blogs', blogRoutes)
+
+app.get('/', function(req, res){
+    res.readFile('index.html')
+});
+
+app.listen(3000);
