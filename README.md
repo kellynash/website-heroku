c4f97d0fe9bce3724f2f0f72e59320c9afd42274
ed42311 <ed42311@gmail.com>
2015-10-22 11:10:27 -0600

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Site-Land
 This is my open site that links to projects, etc
 # site-land
+# website-heroku
--- model/blog.js
+++ model/blog.js
@@ -4,7 +4,6 @@ var Schema       = mongoose.Schema;
 var BlogSchema   = new Schema({
     title: String,
     body: String,
-    id: Number
 });
 
 module.exports = mongoose.model('Blog', BlogSchema);
